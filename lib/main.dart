d3d1e04e1b2918489fc4f70086089972c75efbee
habib-bimh <habib.mahedi007@gmail.com>
2024-02-22 12:06:37 +0600

    tody first

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:food_meals_app/data/dummy_data.dart';
+//import 'package:food_meals_app/data/dummy_data.dart';
 import 'package:food_meals_app/screens/categories.dart';
-import 'package:food_meals_app/screens/meals.dart';
+//import 'package:food_meals_app/screens/meals.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: theme,
       home: const CategoriesScreen(),
     );
